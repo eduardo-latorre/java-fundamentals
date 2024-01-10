c33a828c64169b0fb84b67e62d1a10f121879329
eduardo.latorrevandeperre <eduardo.latorrevandeperre@wbdcontractor.com>
2024-01-10 10:39:48 -0300

    second commit

---
+++ 100-exercices/01-yiels-caculation/YieldCalculation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class YieldCalculation{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double annualRental, propertyValue;
+
+        System.out.print("Enter the anual rental: ");
+        annualRental = scanner.nextDouble();
+        
+        System.out.print("Enter the property value: ");
+        propertyValue = scanner.nextDouble();
+
+        System.out.println("The yield is: " + caculateYield(annualRental, propertyValue));
+
+        scanner.close();
+    }
+
+    public static double caculateYield(double annualRental, double propertyValue){
+        return (annualRental / propertyValue) * 100;
+    }
+
+}
